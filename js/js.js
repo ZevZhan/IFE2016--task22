4a66226d4d805afc035a2c325c3a12cc96ce9526
Wenger <2635329448@qq.com>
2016-04-08 19:44:31 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 //全局变量
 var result = [];  //用于保存二叉树节点
 var timer; 
+var bool;
 
 
 
@@ -11,18 +12,21 @@ window.onload = function(){
 
 	//对按钮进行事件绑定
 	aBtn[0].onclick = function(){
-		if(timer ==1){
+		if(bool == true){
 			return;
 		};	
+		result = [];
 		colorInitial();
 		firOrder(oWrapper);
 		colorChange();
+
 	}
 
 	aBtn[1].onclick = function(){
-		if(timer == 1){
+		if(bool == true){
 			return;
 		};	
+		result = [];
 		colorInitial();
 		midOrder(oWrapper);
 		colorChange();
@@ -31,9 +35,10 @@ window.onload = function(){
 	}
 
 	aBtn[2].onclick = function(){
-		if(timer == 1){
+		if(bool == true){
 			return;
 		};	
+		result = [];
 		colorInitial();
 		lasOrder(oWrapper);
 		colorChange();
@@ -85,20 +90,22 @@ var lasOrder = function(node){
 var colorChange = function(){
 	var i = 0;
 	result[i].style.background = 'green';
-
+	bool = true;
 	timer = setInterval(function(){
 		i++;
 		if(i < result.length){
 			result[i-1].style.background = '#fff ';
 			result[i].style.background = 'green';
 		}else{
+			console.log(i);
 			result[result.length-1].style.background = '#fff';
 			clearInterval(timer);
-			timer = 2;
+			bool = false;
 		}
-	},1000);
+	},500);
 
 	
+
 }
 
 
